047ebc4ea3695ad2f80a4f8236372a7a479bb2dc
manuel.pross <pro_manuel@yahoo.de>
2022-10-28 13:22:17 +0200

    add case for longer arrays

--- assignment_1/functions.py
+++ assignment_1/functions.py
@@ -8,7 +8,7 @@ def calculate_dot_product(vector1, vector2, alpha, beta):
             sum += vector1[i] * vector2[i]
         elif i == 1:
             sum += (vector1[i] * alpha) * (vector2[i] * alpha)
-        elif i == 2:
+        elif i >= 2:
             sum += (vector1[i] * beta) * (vector2[i] * beta)
     return sum
 
@@ -20,6 +20,6 @@ def calculate_length_of_vector(vector, alpha, beta):
             sum += pow(vector[i], 2)
         elif i == 1:
             sum += pow(vector[i] * alpha, 2)
-        elif i == 2:
+        elif i >= 2:
             sum += pow(vector[i] * beta, 2)
     return math.sqrt(sum)
