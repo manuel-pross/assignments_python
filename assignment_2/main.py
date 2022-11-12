bfa708ad255abc0142598a5cb7ace0df12aec753
manuel.pross <pro_manuel@yahoo.de>
2022-11-12 21:47:01 +0100

    first try of reading csv

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -4,14 +4,16 @@ from surprise import BaselineOnly, Dataset, Reader
 from surprise.model_selection import cross_validate
 
 # path to dataset file
-file_path = os.path.expanduser("./BX_Book/BX-Book-Ratings.csv")
+file_path = os.path.expanduser("BX-Book-Ratings.csv")
+
+print(file_path)
 
 # As we're loading a custom dataset, we need to define a reader. In the
 # movielens-100k dataset, each line has the following format:
 # 'user item rating timestamp', separated by '\t' characters.
-reader = Reader(line_format="User-ID;ISBN;Book-Rating", sep=";", skip_lines=1)
+reader = Reader(line_format="user item rating", sep=";", skip_lines=1)
 
-data = Dataset.load_from_file(file_path, reader=reader)
+data = Dataset.load_from_file('./BX-Book-Ratings.csv', reader)
 
 # We can now use this dataset as we please, e.g. calling cross_validate
 cross_validate(BaselineOnly(), data, verbose=True)
